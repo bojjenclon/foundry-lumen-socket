f24c51cf7bcecff0c81dc847b717bd44925ce24f
Cornell Daly <bojjenclon@gmail.com>
2023-09-04 01:21:35 -0400

    Modify the initialization

--- scripts/module.js
+++ scripts/module.js
@@ -1,14 +1,16 @@
-Hooks.once('socketlib.ready', () => {
-  const socket = socketlib.registerModule('lumen-socket')
-  socket.register('essence_used', essence_used)
-  
-  window.lumen = window.lumen || {}
-  window.lumen.socket = socket
-})
-
-function essence_used(id) {
-  const msg = game.messages.find(m => m.id === id)
-  if (msg) {
-    msg.delete()
+Hooks.once('ready', () => {
+  const essence_used = (id) => {
+    const msg = game.messages.find(m => m.id === id)
+    if (msg) {
+      msg.delete()
+    }
   }
-}
+
+  setTimeout(() => {
+    const socket = socketlib.registerModule('lumen-socket')
+    socket.register('essence_used', essence_used)
+    
+    window.lumen = window.lumen || {}
+    window.lumen.socket = socket
+  }, 100)
+})
